75f751bb36825d4a50d45b74f8ef9f7d211a94bc
Jashjeet Singh <jashjeet99@gmail.com>
2026-05-21 00:57:10 +0530

    PUSH operation

---
+++ PUSH.c
@@ -0,0 +1,46 @@
+// PUSH or insertion of number and Display of Stack
+
+# include <stdio.h>
+# include <stdlib.h>
+# define SIZE 5 // Size of Stack
+int top = -1; // Global variable
+
+void pushStack(int stack[], int num) {
+    if(top == SIZE-1) {
+        printf("Stack is full\n");
+        return;
+    }
+    top++;
+    stack[top] = num;
+    
+    return;
+}
+
+void displayStack(int stack[]) {
+    if(top == -1) {
+        printf("Stack is empty\n");
+        return;
+    }
+    printf("Stack elements are");
+    for(int i = top; i>=0; i--) {
+        printf("%d\t", stack[i]);
+    }
+    return;
+}
+
+int main(void) {
+    void pushStack(int [], int);
+    void displayStack(int []);
+    int stack[SIZE-1];
+    int num;
+    int another = 1;
+    while(another == 1) {
+        printf("Enter the number to insert");
+        scanf("%d", &num);
+        pushStack(stack, num);
+        printf("YES/NO 1/0");
+        scanf("%d", &another);
+    }
+    displayStack(stack);
+    return 0;
+}
